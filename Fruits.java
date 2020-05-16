3cb39e9166c85fd9285e455ac20a205c6e28b8e5
yuna-rin-k <g17922ky@gm.tsuda.ac.jp>
2020-05-16 14:16:48 +0900

    simple program

---
+++ Fruits.java
@@ -0,0 +1,9 @@
+public class Fruits {
+	public static void main(String[] args) {
+		
+		String[] fruits = {"Apple", "Banana", "Orange"};
+		System.out.println("Fruits list: ");
+		for (String f : fruits)
+			System.out.println(f);
+	}
+}
